6ef45a04e6015413df1dfbac48ba703be81f7b7b
Matheus-a31 <matheusassis3103@gmail.com>
2025-11-21 12:39:33 -0300

    alterações para banco de dados externo

--- app.py
+++ app.py
@@ -3,19 +3,20 @@ import os
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 
-load_dotenv() 
 
+load_dotenv() 
 
 app = Flask(__name__, template_folder='.')
 app.secret_key = os.getenv("SECRET_KEY") 
 
+database_url = os.getenv("DATABASE_URL", 'sqlite:///site.db') 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-
+# Definição dos Modelos do Banco de Dados (PostgreSQL/SQLite)
 class Parceiro(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
@@ -31,6 +32,7 @@ class Post(db.Model):
 @app.route('/')
 def index():
     
+    
     todos_parceiros = Parceiro.query.all()
     
     parceiros_com_imagem = [p for p in todos_parceiros if p.logo]
@@ -44,16 +46,16 @@ def index():
 
 
 
-
 @app.route('/imagens/<path:filename>')
 def serve_images(filename):
     return send_from_directory(directory='imagens', path=filename)
 
 @app.route('/favicon.ico')
 def favicon():
+    
     return send_from_directory(directory='.', path='favicon.ico')
 
+
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, port=5000)
